d210c5601aa9cf17155f755569e0be233201096d
amrtini <alexandro.martini@pm.me>
2024-05-10 10:03:50 +0200

    Update setup.py to resolve missing packages error upon install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/amyycb/simradrain',
     author='Dr. Amy C. Green',
     author_email='amy.green3@newcastle.ac.uk',
-    packages=['sim', 'est'],
+    packages=['simradrain'],
     install_requires=['numpy', 'pandas', 'scipy', 'gstools', 'matplotlib', 'time', 'timeit', 'h5py', ],
     version='0.1.0',
     description='A Python package for extracting event properties from radar rainfall event data, and simulating '
